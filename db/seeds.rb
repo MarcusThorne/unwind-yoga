279bf0b36cc3a876070fa94dd2560365a9632d4b
Marcus Thorne <marcus_thorne475@hotmail.com>
2023-10-23 13:33:01 +0100

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 Booking.destroy_all
-
 User.destroy_all
+Event.destroy_all
+Review.destroy_all
+
 User.create(name: 'Marcus', email: 'admin@admin.com', password: '123456', admin: true, confirmed_at: DateTime.current)
 User.create(name: 'Marcus', email: 'user@admin.com', password: '123456', admin: false, confirmed_at: DateTime.current)
-puts 'Users created'
 
 Event.create(name: 'Yoga', description: 'Deep stretch yoga')
 Event.create(name: 'Full Body', description: 'Full body workout')
