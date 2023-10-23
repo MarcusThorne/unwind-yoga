b26a7d87af8dd78b51de4bee6dee481672434160
Marcus Thorne <marcus_thorne475@hotmail.com>
2023-10-23 13:44:18 +0100

    dont use pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Disable serving static files from the `/public` folder by default since
+  # Apache or NGINX already handles this.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
